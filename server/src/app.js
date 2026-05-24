0520b59132262fe5319fee2610092ec1e427eccb
jioryII <yajaeltra@gmail.com>
2026-05-24 02:11:11 -0500

    Fix: Usar RegExp para ruta catch-all compatible con Express v5

--- server/src/app.js
+++ server/src/app.js
@@ -44,7 +44,7 @@ app.get('/api/health', (req, res) => {
 app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // Manejar cualquier otra ruta con el index.html del frontend (SPA)
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
